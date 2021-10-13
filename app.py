bf0273bd39c615ddf6c1e0ede8b0de4139ebbf72
santiago11990 <92319800+santiago11990@users.noreply.github.com>
2021-10-12 22:17:28 -0500

    Se agregaron métodos GET y POST

--- app.py
+++ app.py
@@ -6,47 +6,47 @@ app = Flask(__name__)
 app.secret_key=os.urandom(24)
 
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def index():
     return render_template('index.html')
 
-@app.route('/about')
+@app.route('/about', methods=["GET"])
 def about():
     return render_template('Acerca-de.html')
 
-@app.route('/bebidas')
+@app.route('/bebidas', methods=["GET"])
 def getBebidas():
     return render_template('bebidas_list.html')
 
-@app.route('/contacto')
+@app.route('/contacto', methods=["GET","POST"])
 def contacto():
     return render_template('contacto.html')
 
-@app.route('/detalle_plato')
+@app.route('/detalle_plato', methods=["GET"])
 def detalle_plato():
     return render_template('detalle_plato.html')
 
-@app.route('/register')
+@app.route('/register', methods=["GET","POST"])
 def register():
     return render_template('registro.html')
 
-@app.route('/login')
+@app.route('/login', methods=["GET","POST"])
 def login():
     return render_template('inicio_sesion.html')
 
-@app.route('/menu')
+@app.route('/menu', methods=["GET"])
 def getMenu():
     return render_template('menu_list.html')
 
-@app.route('/update_user')
+@app.route('/update_user', methods=["GET","POST"])
 def update_user():
     return render_template('usuario_update_form.html')
 
-@app.route('/update_bebida')
+@app.route('/update_bebida', methods=["GET","POST"])
 def update_bebida():
     return render_template('bebida_update_form.html')
 
-@app.route('/usuarios')
+@app.route('/usuarios', methods=["GET"])
 def getUsuarios():
     return render_template('usuarios_list.html')
 
