380e5009b0c43bc6879b180acbc6a4a62403ca45
unknown <bijoe@uninorte.edu.co>
2021-10-14 09:47:03 -0500

    archivo para validar registro sin funcionar

---
+++ static/js/biojoe.js
@@ -0,0 +1,38 @@
+function validar_formulario(){
+    let username = document.formRegistro.username;
+    let email = document.formRegistro.email;
+    let password = document.formRegistro.password;
+
+    let username_len = username.value.length;
+
+    if(username_len == 0 || username_len < 8){
+        alert("Debes ingresar un username con minímo 8 caracteres");
+        username_len.focus(); // No debe borrar los datos.
+        return false;
+    }
+
+    let formato_email = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/;
+    if(!email.value.match(formato_email)){
+        alert("Debes ingresar un email valido!");
+        email.focus();
+        return false;
+    }
+
+    let pass_len = password.value.length;
+    if( pass_len == 0 || pass_len < 8){
+        alert("Debes ingresar un password con más de 8 caracteres");
+        return false;
+    }
+    
+}
+
+// Muestra la clave al pasar el mouse sobre la etiqueta img.
+function mostrarPassword(obj){
+    var obj = document.getElementById("password");
+    obj.type="text";
+}
+
+function ocultarPassword(obj){
+    var obj = document.getElementById("password");
+    obj.type="password";
+}
